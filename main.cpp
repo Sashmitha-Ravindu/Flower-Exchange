c0dbf4dc8812230f267540a7603c9922f7c82dce
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2023-05-28 08:27:57 +0530

    Make orderbooks regarding to each instrument

--- main.cpp
+++ main.cpp
@@ -9,13 +9,14 @@ using namespace std;
 
 
 bool isOrderValid(string clOrdId, string instrument, string side, string quantity, string price) {
-    string instrumentList[]={"Rose","Lavender","Lotus","Tulip","Orchid"};
+    string instrumentList[] = {"Rose", "Lavender", "Lotus", "Tulip", "Orchid"};
     return !clOrdId.empty() &&
            !instrument.empty() &&
            !side.empty() &&
            !quantity.empty() &&
            !price.empty() &&
-            (instrument=="Rose" || instrument=="Lavender" || instrument=="Lotus" || instrument=="Tulip" || instrument=="Orchid")&&
+           (instrument == "Rose" || instrument == "Lavender" || instrument == "Lotus" || instrument == "Tulip" ||
+            instrument == "Orchid") &&
            (stoi(side) == 1 || stoi(side) == 2) &&
            stod(price) > 0.0 &&
            stoi(quantity) % 10 == 0 &&
@@ -57,17 +58,33 @@ int main() {
         orderList.push_back(order1);
     }
 
+    vector<vector<Order>> roseOrderBook;
+    vector<vector<Order>> lavenderOrderBook;
+    vector<vector<Order>> lotusOrderBook;
+    vector<vector<Order>> tulipOrderBook;
+    vector<vector<Order>> orchidOrderBook;
+
 
     ofstream outData;
-    string s;
-
-
-    int n = 1;
-
     outData.open("../execution_rep.csv", ofstream::out | ofstream::trunc);
     outData << "Order ID,Client Order,Instrument,Side,Exec Status,Quantity,Price,Reason" << endl;
-    for (int i = 0; i < orderList.size(); ++i) {
-        outData << orderList[i].getDetails() << endl;
+//    for (int i = 0; i < orderList.size(); ++i) {
+//        outData << orderList[i].getDetails() << endl;
+//    }
+
+    for (int i = 0; i < orderList.size(); i++) {
+        string type = orderList[i].getInstrument();
+        if (type == "Rose") {
+            roseOrderBook[orderList[i].getSide() - 1].push_back(orderList[i]);
+        } else if (type == "Lavender") {
+            lavenderOrderBook[orderList[i].getSide() - 1].push_back(orderList[i]);
+        } else if (type == "Lotus") {
+            lotusOrderBook[orderList[i].getSide() - 1].push_back(orderList[i]);
+        } else if (type == "Tulip") {
+            tulipOrderBook[orderList[i].getSide() - 1].push_back(orderList[i]);
+        } else if (type == "Orchid") {
+            orchidOrderBook[orderList[i].getSide() - 1].push_back(orderList[i]);
+        }
     }
 
     outData.close();
